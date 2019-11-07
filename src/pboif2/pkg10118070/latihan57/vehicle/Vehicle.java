862f4982006faa5059b78c83ccdaa08c13d9144b
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-11-07 21:49:37 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan57/vehicle/Vehicle.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan57.vehicle;
+
+/**
+ *
+ * @author Ailyasha
+ * NAMA     : Aufa Ilyasha
+ * Kelas    : IF2
+ * NIM      : 10118070
+ */
+class Vehicle {
+    private String myBrand, myModel;
+    
+    public Vehicle() {
+        
+    }
+
+    public String getMyBrand() {
+        return myBrand;
+    }
+
+    public void setMyBrand(String myBrand) {
+        this.myBrand = myBrand;
+    }
+
+    public String getMyModel() {
+        return myModel;
+    }
+
+    public void setMyModel(String myModel) {
+        this.myModel = myModel;
+    }
+    
+}
